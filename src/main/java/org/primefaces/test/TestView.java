d474e4b4dcf900761b26b3a552a71300ee444ed3
rmm <rmm@RMMbook.localdomain>
2020-03-23 11:03:15 -0500

    demo

--- src/main/java/org/primefaces/test/TestView.java
+++ src/main/java/org/primefaces/test/TestView.java
@@ -1,6 +1,9 @@
 package org.primefaces.test;
 
 import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.List;
+
 import javax.annotation.PostConstruct;
 import javax.faces.view.ViewScoped;
 import javax.inject.Named;
@@ -8,13 +11,33 @@ import javax.inject.Named;
 @Named
 @ViewScoped
 public class TestView implements Serializable {
-    
+
     private String testString;
-    
-    @PostConstruct  
+
+    private List<String> list = new ArrayList<>();
+
+    public List<String> getList() {
+		return list;
+	}
+
+	public void setList(List<String> list) {
+		this.list = list;
+	}
+
+	@PostConstruct
     public void init() {
         testString = "Welcome to PrimeFaces!!!";
+        for (int i = 0; i < 50; i++) {
+			list.add("a" + 1);
+		}
     }
+
+	public void add() {
+        for (int i = 0; i < 50; i++) {
+			list.add("a" + 1);
+		}
+
+	}
 
     public String getTestString() {
         return testString;
@@ -22,5 +45,5 @@ public class TestView implements Serializable {
 
     public void setTestString(String testString) {
         this.testString = testString;
-    }    
+    }
 }
